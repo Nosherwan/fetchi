c2fe7b089a82f70cc191298187a4b3b3cf64bfba
Nosh Ghazanfar <nosherwan@gmail.com>
2016-08-24 22:41:24 +1000

    1. Eslintrc introduced. 2. code linted and modified. WIP

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*eslint-disable no-console*/
+
 'use strict';
 
 // The value for globalUri should be set before using Fetchi.fetch
@@ -13,60 +15,57 @@ function _queryStringFromObject(api, data) {
 	queryString = (api && queryString) ? '?' + queryString : (queryString ? queryString : '');
 
 	return api ? (_globalUri + api + queryString) : queryString;
-};
+}
 
 function _queryUri(api) {
 	return _globalUri + api;
-};
+}
+
 /**
- * Convenience method for fetch requests
- * @param {Object} options 
- * @return {Object} promise
- */
+* Convenience method for fetch requests
+* @param {Object} options 
+* @return {Object} promise
+*/
 function _fetch(options) {
-		options = options || {};
-		options.method = options.method || 'get';
-		options.data = options.data || {};
-		var params = {
-			method: 'get',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: ''
+	var promise = null;
+	var params = { method: '', headers: {} };
+
+	options = options || {};
+	options.method = options.method || 'get';
+	options.data = options.data || {};
+
+	params.method = options.method;
+	if (options.type === 'json') {
+		params.headers = {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
 		};
-		var promise = null;
+	}
 
-		if (options.authorization) {
-			params.headers['Authorization'] = options.authorization;
-		}
+	if (options.authorization) {
+		params.headers['Authorization'] = options.authorization;
+	}
 
-		if (options.api) {
-			switch (options.method) {
-				case 'post':
-				case 'put':
-					params.method = options.method;
-					if (options.type !== 'json') {
-						params.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-						params.body = _queryStringFromObject(null, options.data);
-					} else {
-						params.body = JSON.stringify(options.data);
-					}
-					promise = fetch(_queryUri(options.api), params);
-					break;
-				default:    //should serve for get
-					promise = fetch(_queryStringFromObject(options.api, options.data), {
-						method: params.method, headers: params.headers
-					});
-					break;
+	if (options.api) {
+		switch (options.method) {
+		case 'post':
+		case 'put':
+			params.method = options.method;
+			if (options.type !== 'json') {
+				params.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+				params.body = _queryStringFromObject(null, options.data);
+			} else {
+				params.body = JSON.stringify(options.data);
 			}
-
-			return promise.then(function (result) {
-				return result.json()
+			return fetch(_queryUri(options.api), params);
+		default:    //should serve for get
+			return fetch(_queryStringFromObject(options.api, options.data), {
+				method: params.method, headers: params.headers
 			});
-		} else {
-			console.log('Please atleast provide an api endpoint for fetch to work.');
 		}
+	} else {
+		console.log('Please atleast provide an api endpoint for fetch to work.');
+	}
 }
 
 var Reesorce = {
@@ -82,13 +81,30 @@ var Reesorce = {
 		//TODO:	Needs implementation
 	},
 
-	getJSON: function (options) {
-		//TODO:	Needs implementation
+	getJSON: function (endPoint, options) {
+		options.type = 'json';
+		options.method = 'get';
+		options.api = endPoint;
+		return _fetch(options)
+			.then(function (result) {
+				return result.json();
+			});
 	},
 
-	postJSON: function (options) {
-		//TODO:	Needs implementation
+	postJSON: function (endPoint, options) {
+		options.type = 'json';
+		options.method = 'post';
+		options.api = endPoint;
+		return _fetch(options)
+			.then(function (result) {
+				return result.json();
+			});
+	},
+
+	postForm: function(options){
+
 	}
+	
 };
 
 module.exports = Reesorce;
